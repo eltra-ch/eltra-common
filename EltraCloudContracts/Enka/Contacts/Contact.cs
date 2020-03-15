c4e3ce7cdd2b2f639f74ebc20cd650826f0d9dd9
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-03-15 09:06:16 +0100

    fixes #24

---
+++ EltraCloudContracts/Enka/Contacts/Contact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace EltraCloudContracts.Enka.Contacts
+{
+    [DataContract]
+    public class Contact
+    {
+        #region Properties
+
+        [DataMember]
+        [Required]
+        public string FirstName { get; set; }
+        [DataMember]
+        [Required]
+        public string LastName { get; set; }
+        [DataMember]
+        public string PhoneStationary { get; set; }
+        [DataMember]
+        public string PhoneMobile { get; set; }
+        [DataMember]
+        public string Street { get; set; }
+        [DataMember]
+        public string HouseNumber { get; set; }
+        [DataMember]
+        public string FlatNumber { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public string PostalCode { get; set; }
+        [DataMember]
+        public string Notice { get; set; }
+        [DataMember]
+        public DateTime Modified { get; set; }
+        [DataMember]
+        public DateTime Created { get; set; }
+        #endregion
+    }
+}
