10fe10b2210f7ade9eae921b1146d70bad68fbf4
Dawid <dawid@eltra.ch>
2020-03-16 08:15:06 +0100

    fixes #24

---
+++ EltraCloudContracts/Enka/Orders/OrderInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+using EltraCloudContracts.Enka.Contacts;
+
+namespace EltraCloudContracts.Enka.Orders
+{
+    [DataContract]
+    public class OrderInfo
+    {
+        [DataMember]
+        public Order Order { get; set; }
+        [DataMember]
+        public Contact CreatedBy { get; set; }
+    }
+}
