fd115c23ebdd369fcdff2a55c3a4bef8c485d229
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-03-18 21:53:23 +0100

    enka telephone number check #28

---
+++ EltraCommon/Helpers/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EltraCommon.Helpers
+{
+    public static class StringExtensions
+    {
+        public static bool IsPhoneNumber(this string number)
+        {
+            return Regex.Match(number, @"^(\+[0-9]{9})$").Success;
+        }
+    }
+}
