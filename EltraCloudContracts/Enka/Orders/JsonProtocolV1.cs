51a155b2b98dcf06ee8b8617fed44e654dbad0fb
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-03-22 19:39:34 +0100

    json protocol v1

---
+++ EltraCloudContracts/Enka/Orders/JsonProtocolV1.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace EltraCloudContracts.Enka.Orders
+{
+    [DataContract]
+    public class JsonProtocolV1
+    {
+        [DataMember]
+        public bool Car { get; set; }
+        [DataMember]
+        public bool Shop { get; set; }
+        [DataMember]
+        public bool DrugStore { get; set; }
+        [DataMember]
+        public bool Other { get; set; }
+    }
+}
