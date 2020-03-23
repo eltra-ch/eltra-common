eea784b6f1c1bf7846ab09429e06ed0e759ee6d4
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-03-23 21:11:55 +0100

    phone number validation

--- EltraCommon/Helpers/StringExtensions.cs
+++ EltraCommon/Helpers/StringExtensions.cs
@@ -1,12 +1,66 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
 
 namespace EltraCommon.Helpers
 {
     public static class StringExtensions
     {
-        public static bool IsPhoneNumber(this string number)
+        public static bool IsPhoneNumber(this string phoneNumber)
         {
-            return Regex.Match(number, @"^(\+[0-9]{9})$").Success;
+            const int RegionalPhoneLength = 9;
+            const int InternationalPhoneLength = 11;
+
+            string normalized = NormalizePhoneNumber(phoneNumber);
+
+            return normalized.Length == RegionalPhoneLength || normalized.Length == InternationalPhoneLength;
         }
+
+        private static string NormalizePhoneNumber(string phoneNumber)
+        {
+            string result = string.Empty;
+
+            if (!string.IsNullOrEmpty(phoneNumber))
+            {
+                phoneNumber = phoneNumber.Replace(" ", string.Empty);
+
+                if (phoneNumber.StartsWith("+"))
+                {
+                    phoneNumber = phoneNumber.Insert(0, "00");
+                }
+
+                var entities = phoneNumber.Split(new[] { ' ', '-', '+', '(', ')', '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string normalizedPhoneNum = string.Empty;
+
+                foreach (var entity in entities)
+                {
+                    foreach (var c in entity)
+                    {
+                        if (char.IsDigit(c))
+                        {
+                            normalizedPhoneNum += c;
+                        }
+                    }
+                }
+
+                if (normalizedPhoneNum.StartsWith("0"))
+                {
+                    int firstNotZero = 0;
+                    foreach (var c in normalizedPhoneNum)
+                    {
+                        if (c != '0')
+                        {
+                            break;
+                        }
+
+                        firstNotZero++;
+                    }
+
+                    result = normalizedPhoneNum.Substring(firstNotZero);
+                }
+            }
+
+            return result;
+        }
+
     }
 }
