b555bd157a8002eedebb8b3b55d1870fc7eeca08
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-04-30 17:53:47 +0200

    voting system #3

---
+++ EltraCloudContracts/Enka/Contacts/Rating.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EltraCloudContracts.Enka.Contacts
+{
+    [DataContract]
+    public class Rating
+    {
+        [DataMember]
+        public double MinValue { get => 0; }
+        [DataMember]
+        public double MaxValue { get => 5; }
+        [DataMember]
+        public double Value { get; set; }
+        [DataMember]
+        public DateTime Created { get; set; }
+        
+        [IgnoreDataMember]
+        public bool InRange
+        {
+            get 
+            {
+                bool result = false;
+
+                if (Value > MinValue && Value <= MaxValue)
+                {
+                    result = true;
+                }
+
+                return result;
+            }
+        }
+    }
+}
