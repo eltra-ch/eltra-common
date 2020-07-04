539b0d0dc2d869fb100c07a61c00e72d2d599812
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-07-04 08:58:06 +0200

    sessions

---
+++ EltraCommon/Contracts/Sessions/Sessions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace EltraCommon.Contracts.Sessions
+{
+    [DataContract]
+    public class Sessions
+    {
+        private List<SessionDevices> _sessionDevices;
+
+        [DataMember]
+        public List<SessionDevices> SessionDevices
+        {
+            get => _sessionDevices ?? (_sessionDevices = new List<SessionDevices>());
+        }
+    }
+}
