2a1682904eed7d06d40888d0bddf3a80b7773a73
Dawid <dawid@eltra.ch>
2020-08-17 10:21:01 +0200

    ParameterValueUpdate

---
+++ EltraCommon/Contracts/Parameters/ParameterValueUpdate.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using EltraCommon.ObjectDictionary.Common.DeviceDescription.Profiles.Application.Parameters;
+
+namespace EltraCommon.Contracts.Parameters
+{
+    /// <summary>
+    /// ParameterValueUpdate
+    /// </summary>
+    [DataContract]
+    public class ParameterValueUpdate
+    {
+        /// <summary>
+        /// ChannelId
+        /// </summary>
+        [DataMember]
+        public string ChannelId { get; set; }
+        /// <summary>
+        /// NodeId
+        /// </summary>
+        [DataMember]
+        public int NodeId { get; set; }
+        /// <summary>
+        /// Parameter
+        /// </summary>
+        [DataMember]
+        public ParameterValue ParameterValue { get; set; }
+}
+}
