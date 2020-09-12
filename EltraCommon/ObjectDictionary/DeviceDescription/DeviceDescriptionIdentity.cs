7323e2a285d979483debe0d4013cb201440cd28e
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-09-12 19:49:52 +0200

    cache device description file #14

---
+++ EltraCommon/ObjectDictionary/DeviceDescription/DeviceDescriptionIdentity.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace EltraCommon.ObjectDictionary.DeviceDescription
+{
+    /// <summary>
+    /// Device Description Identity
+    /// </summary>
+    [DataContract]
+    public class DeviceDescriptionIdentity
+    {
+        /// <summary>
+        /// Hash code encoding algorithm
+        /// </summary>
+        [DataMember]
+        public string Encoding { get; set; }
+
+        /// <summary>
+        /// Hash code content
+        /// </summary>
+        [DataMember]
+        public string Content { get; set; }
+    }
+}
