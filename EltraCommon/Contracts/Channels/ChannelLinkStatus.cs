c70178fe85ff24b5e1eff089f1856bed98cd7552
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-10-04 08:07:11 +0200

    unbind method #115

---
+++ EltraCommon/Contracts/Channels/ChannelLinkStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace EltraCommon.Contracts.Channels
+{
+    /// <summary>
+    /// ChannelLinkStatus
+    /// </summary>
+    public enum ChannelLinkStatus
+    {
+        /// <summary>
+        /// Undefined
+        /// </summary>
+        Undefined,
+        /// <summary>
+        /// Unbound
+        /// </summary>
+        Unbound,
+        /// <summary>
+        /// Binded
+        /// </summary>
+        Binded,
+    }
+}
